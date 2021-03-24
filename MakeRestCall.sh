8a40e1573ffbb26184d7c11e7135d16b4f2ff70f
Jyotsna <Josh-01@github.com>
2021-03-24 18:41:38 +0530

    Update MakeRestCall.sh

--- MakeRestCall.sh
+++ MakeRestCall.sh
@@ -1,7 +1,9 @@
 token=$1
 
+code=$(curl GET https://github.com/login/oauth/authorize?client_id=9e4fedaa995f29228ee)
+access_code=&(curl X POST -H https://github.com/login/oauth/access_token?client_id=9e4fedaa995f29228ee&client_secret=ef8c49d9267f66a2278a5007fc6d9ac5f2e605ce&code=$code)
 
-response=$(curl X GET -H josh-01:$token https://api.github.com/repos/Josh-01/hello-kubernetes/actions/secrets)
+response=$(curl X GET -H Authorization: token $access_code https://api.github.com/repos/Josh-01/hello-kubernetes/actions/secrets)
 
 if [ -n "$response" ] && [ -n "$response.total_count" ]; then
     echo "REST call made successfully: '$response'"
