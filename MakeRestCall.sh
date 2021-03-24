dcea73eae4ebb412dbbd72a429f57c9f7b97b5a1
Jyotsna <Josh-01@github.com>
2021-03-24 14:45:28 +0530

    Update MakeRestCall.sh

--- MakeRestCall.sh
+++ MakeRestCall.sh
@@ -2,7 +2,7 @@ token=$1
 
 response=$(curl -X GET -H "Authorization: token $token" https://api.github.com/repos/Josh-01/hello-kubernetes/actions/secrets)
 
-if [ "$response" == "" ]; then
+if [ "$response" != "" &&  "$response.total_count" != "" ]; then
     echo "REST call made successfully"
 else
     echo "REST call failed with: '$response'"
