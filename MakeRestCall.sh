3cbec29322a172f76031bef50368a8decd6dbe5f
Jyotsna <Josh-01@github.com>
2021-03-24 19:01:57 +0530

    Create MakeRestCall.sh

---
+++ MakeRestCall.sh
@@ -0,0 +1,11 @@
+token=$1
+
+response=$(curl -X GET -H "josh-01:$token"  https://api.github.com/repos/Josh-01/hello-kubernetes/branches)
+
+if [ -n "$response" ] && [ -n "$response.total_count" ]; then
+    echo "REST call made successfully: '$response'"
+    exit 0
+else
+    echo "REST call failed with: '$response'"
+    exit 1
+fi
