0d8860106447a3214e39a7603779f7166a6e8225
Jyotsna <Josh-01@github.com>
2021-03-24 18:50:45 +0530

    Update MakeRestCall.sh

--- MakeRestCall.sh
+++ MakeRestCall.sh
@@ -1,7 +1,7 @@
 token=$1
 
 code=$(curl -X GET -H https://github.com/login/oauth/authorize?client_id=9e4fedaa995f29228ee)
-access_code=&(curl -X POST -H https://github.com/login/oauth/access_token?client_id=9e4fedaa995f29228ee&client_secret=ef8c49d9267f66a2278a5007fc6d9ac5f2e605ce&code=$code)
+access_code=$(curl -X POST -H https://github.com/login/oauth/access_token?client_id=9e4fedaa995f29228ee&client_secret=ef8c49d9267f66a2278a5007fc6d9ac5f2e605ce&code=$code)
 
 response=$(curl -X GET -H "Authorization: $access_code" https://api.github.com/repos/Josh-01/hello-kubernetes/actions/secrets)
 
