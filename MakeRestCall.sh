2913c0c290a65ffb3c32e1bea91a79190bf222d1
Jyotsna <Josh-01@github.com>
2021-03-25 19:19:05 +0530

    Update MakeRestCall.sh

--- MakeRestCall.sh
+++ MakeRestCall.sh
@@ -1,6 +1,6 @@
 token=$1
 
-response=$(curl -X GET -H "josh-01:$token"  https://api.github.com/repos/Josh-01/hello-kubernetes/branches)
+response=$(curl -U josh-01:$token -X GET https://api.github.com/repos/Josh-01/hello-kubernetes/branches)
 
 if [ -n "$response" ] && [ -n "$response.total_count" ]; then
     echo "REST call made successfully: '$response'"
